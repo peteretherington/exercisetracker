454f14e56e6db582a4538b97161e3dbba8da317f
Peter Etherington <pjg.etherington@gmail.com>
2020-01-10 11:08:47 -0500

    Completed post user and get users

--- server.js
+++ server.js
@@ -1,21 +1,83 @@
 require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-
-const cors = require('cors');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+	console.log(mongoose.connection.readyState)
+);
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
+});
+
+// Schema
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({ username: String });
+
+const ExerciseSchema = new Schema({
+	userId: String,
+	description: String,
+	duration: String,
+	date: String
+});
+
+// Model
+const model = mongoose.model;
+
+const UserModel = model('User', UserSchema);
+const ExerciseModel = model('Exercise', ExerciseSchema);
+
+// I can create a user by posting form data username to /api/exercise/new-user and returned
+// will be an object with username and _id.
+app.post('/api/exercise/new-user', (req, res) => {
+	const { username } = req.body;
+	const User = new UserModel({ username });
+
+	UserModel.findOne({ username }, (err, user) => {
+		if (err) res.json(err);
+		if (user) res.json({ message: `User <${user.username}> already exists.` });
+		if (!user) {
+			User.save((err, { _id, username }) => (err ? res.json(err) : res.json({ _id, username })));
+		}
+	});
+});
+
+// I can get an array of all users by getting api/exercise/users with the same info as
+// when creating a user.
+app.get('/api/exercise/users', (req, res) => {
+	UserModel.find({}, (err, users) => {
+		if (err) res.json(err);
+		res.json({
+			users: users.reduce((acc, { _id, username }) => [...acc, { _id, username }], [])
+		});
+	});
+});
+
+// I can add an exercise to any user by posting form data userId(_id), description, duration,
+// and optionally date to /api/exercise/add. If no date supplied it will use current date.
+// App will return the user object with the exercise fields added.
+app.post('/api/exercise/add', (req, res) => {
+	const { userId, description, duration, date } = req.body;
+	res.json({ userId, description, duration, date });
+});
+
+// 1) I can retrieve a full exercise log of any user by getting /api/exercise/log with a
+// parameter of userId(_id). App will return the user object with added array log and count
+// (total exercise count).
+// 2) I can retrieve part of the log of any user by also passing along optional parameters
+// of from & to or limit. (Date format yyyy-mm-dd, limit = int)
+app.get('/api/exercise/log?{userId}[&from][&to][&limit]', (req, res) => {
+	console.log('GET exercise log endpoint');
+	res.json({ message: 'GET exercise log endpoint' });
 });
 
 // Not found middleware
